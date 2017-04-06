6e457818e503c1381dbb2873f9f18888b998303f
Cedric Guerin <guerin.ced@gmail.com>
2017-04-06 13:55:29 +0200

    Add Races dropdown to navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,59 +1,85 @@
+import { Component } from 'react'
 import { strava_login_url } from '../../config/config'
 import { Link } from 'react-router'
 import './navbar.less'
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
 
-const AppNavbar = ({athlete, isAuthenticated, onClickProfile}) => {
+export default class AppNavbar extends Component {
 
-    let athleteInformation
-    const clickProfile = () => {
-        onClickProfile(athlete.id)
+    constructor() {
+        super()
+        this.state = {
+            raceDropdownOpen: false
+        }
     }
 
-    if (isAuthenticated) {
-        const ownStyle = {
-            backgroundImage: `url(${athlete.profile_medium})`
-        }
-        athleteInformation = (
-            <div>
-                <Link to={'/athlete'} onClick={clickProfile}>
-                    <div style={ownStyle} className="avatar">
+    toggle() {
+        this.setState({raceDropdownOpen: !this.state.raceDropdownOpen})
+    }
+
+    clickProfile() {
+        this.props.onClickProfile(this.props.athlete.id)
+    }
+
+    isAuthenticatedNavBarLink() {
+        return (
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <ButtonDropdown isOpen={this.state.raceDropdownOpen} toggle={this.toggle.bind(this)}>
+                        <DropdownToggle caret>
+                            Races
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem><Link to={'/'}>Create race</Link></DropdownItem>
+                            <DropdownItem divider/>
+                            <DropdownItem>Another Action</DropdownItem>
+                        </DropdownMenu>
+                    </ButtonDropdown>
+                </NavItem>
+                <NavItem>
+                    <Link to={'/timeline'}>
+                        Timeline
+                    </Link>
+                </NavItem>
+                < NavItem >
+                    <div>
+                        <Link to={'/athlete'} onClick={this.clickProfile}>
+                            <div style={{backgroundImage: `url(${this.props.athlete.profile_medium})`}}
+                                 className="avatar">
+                            </div>
+                        </Link>
                     </div>
-                </Link>
-            </div>
+                </NavItem>
+            </Nav>
         )
     }
 
-    return (
-        <Navbar color="faded" light toggleable>
-            <NavbarToggler right onClick={null/*this.toggle*/}/>
-            <NavbarBrand>
-                <Link to={'/'}>
-                    World-Cup
-                </Link>
-            </NavbarBrand>
-            <Collapse isOpen={false/*this.state.isOpen*/} navbar>
-                <Nav className="ml-auto" navbar>
-                    <NavItem>
-                        <NavLink>
-                            <Link to={'/timeline'}>
-                                Timeline
-                            </Link>
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        {!isAuthenticated && (
-                            <a href={strava_login_url}>
-                                <button className="btn btn-sm btn-outline-warning my-2 my-sm-0" type="submit">Connect
-                                </button>
-                            </a>
-                        )}
-                        {athleteInformation}
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
-    )
-}
+    isNotAuthenticatedNavBarLink() {
+        return (
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <a href={strava_login_url}>
+                        <button className="btn btn-sm btn-outline-warning my-2 my-sm-0" type="submit">Connect
+                        </button>
+                    </a>
+                </NavItem>
+            </Nav>
+        )
+    }
 
-export default AppNavbar
+    render() {
+        return (
+            <Navbar color="faded" light toggleable>
+                <NavbarToggler right onClick={null/*this.toggle*/}/>
+                <NavbarBrand>
+                    <Link to={'/'}>
+                        World-Cup
+                    </Link>
+                </NavbarBrand>
+                <Collapse isOpen={false/*this.state.isOpen*/} navbar>
+                    {this.props.isAuthenticated && this.isAuthenticatedNavBarLink() || this.isNotAuthenticatedNavBarLink()}
+                </Collapse>
+            </Navbar>
+        )
+    }
+}
