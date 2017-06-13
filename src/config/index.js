a80f9e0140eb8480764e73401fe86cbd79f4703b
Cedric Guerin <guerin.ced@gmail.com>
2017-06-13 21:07:35 +0200

    Adding production config

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,10 @@ const config = {
         strava_login_url: "http://www.strava.com/oauth/authorize?client_id=18363&response_type=code&redirect_uri=http://127.0.0.1:3333/login&approval_prompt=auto&scope=view_private",
         api_url: "http://localhost:15000"
     },
-    production: {}
+    production: {
+        strava_login_url: "http://www.strava.com/oauth/authorize?client_id=18363&response_type=code&redirect_uri=http://127.0.0.1:3333/login&approval_prompt=auto&scope=view_private",
+        api_url: "http://api.worldcup.guericed.com"
+    }
 }
 
 export default config[window.environment]
