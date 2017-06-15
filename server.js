87ee2f4e98419ab0fc83e2aa2bf255ab6385b914
Cedric Guerin <guerin.ced@gmail.com>
2017-06-15 13:13:11 +0200

    Add all react-router routes in server to load app on browser F5

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express()
 
 app.use('/', express.static('dist'))
 app.use('/login', express.static('dist'))
+app.use('/timeline', express.static('dist'))
+app.use('/athlete', express.static('dist'))
+app.use('/create-race', express.static('dist'))
 
 app.use((req, res) => {
     res.status(404).send('Page Introuvable!')
